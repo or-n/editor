6dec4c49d8290321fa47d35a66840ebb7e4b4b1c
or-n <kulombowie@gmail.com>
2024-10-30 17:38:32 +0100

    token fix apply

--- term_token/apply.rs
+++ term_token/apply.rs
@@ -14,16 +14,14 @@ pub struct Term(pub BTerm);
 impl Eat<Token, Error, ()> for Term {
     fn eat(i: &[Token], _data: ()) -> Result<(&[Token], Self), Error> {
         use Error::*;
-        let (i, a) = BTerm::eat(
-            i,
-            Settings {
-                apply: false,
-                ..Settings::all(true)
-            },
-        )
-        .map_err(A)?;
+        let (i, a) = BTerm::eat(i, Settings::all(false)).map_err(A)?;
         let i = Token::Whitespace(' ', 1).drop(i).map_err(|_| Space)?;
-        let (i, b) = BTerm::eat(i, Settings::all(false)).map_err(B)?;
-        Ok((i, Self(apply(a, b))))
+        let (i, b) = BTerm::eat(i, Settings::all(true)).map_err(B)?;
+        use crate::term::Term::*;
+        let t = match *b {
+            Apply(b1, b2) => apply(apply(a, b1), b2),
+            _ => apply(a, b),
+        };
+        Ok((i, Self(t)))
     }
 }
